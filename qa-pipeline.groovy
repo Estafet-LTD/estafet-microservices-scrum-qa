3f83cf8715a4617b3a75d540ba288e817aecd8e0
Dennis Williams <dennis.williams@estafet.com>
2018-05-03 12:12:37 +0100

    added new pipeline for qa acceptance tests

---
+++ qa-pipeline.groovy
@@ -0,0 +1,15 @@
+node() {
+
+	def project = "test"
+
+	stage("checkout") {
+		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-qa"
+	}
+
+	stage("execute acceptance tests") {
+		sh "mvn clean install"
+		junit "**/target/surefire-reports/*.xml"	
+	}
+
+}
+
