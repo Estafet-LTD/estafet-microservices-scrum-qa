c20b1a584b666f2688da35c37b678664a84a983d
Dennis Williams <dennis.williams@estafet.com>
2018-05-21 19:32:13 +0100

    correct the urls for the environment variables.

--- qa-pipeline.groovy
+++ qa-pipeline.groovy
@@ -1,37 +1,35 @@
 node('maven') {
 
-	def project = "test"
-
 	stage("checkout") {
 		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-qa"
 	}
 
 	stage("unit tests") {
-		withEnv( [ 	"BASIC_UI_URI=http://${microservice}.${project}.svc:8080",
-								"TASK_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+		withEnv( [ 	"BASIC_UI_URI=http://basic-ui.test.svc:8080",
+								"TASK_API_JDBC_URL=jdbc:postgresql://postgresql.test.svc:5432/task-api", 
 								"TASK_API_DB_USER=postgres", 
 								"TASK_API_DB_PASSWORD=welcome1",
-								"TASK_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
-								"STORY_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"TASK_API_SERVICE_URI=http://task-api.test.svc:8080",
+								"STORY_API_JDBC_URL=jdbc:postgresql://postgresql.test.svc:5432/story-api", 
 								"STORY_API_DB_USER=postgres", 
 								"STORY_API_DB_PASSWORD=welcome1",
-								"STORY_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
-								"SPRINT_BURNDOWN_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"STORY_API_SERVICE_URI=http://${microservice}.test.svc:8080",
+								"SPRINT_BURNDOWN_JDBC_URL=jdbc:postgresql://postgresql.test.svc:5432/sprint-burndown", 
 								"SPRINT_BURNDOWN_DB_USER=postgres", 
 								"SPRINT_BURNDOWN_DB_PASSWORD=welcome1",
-								"SPRINT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}.svc:8080",
-								"SPRINT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"SPRINT_BURNDOWN_SERVICE_URI=http://sprint-burndown.test.svc:8080",
+								"SPRINT_API_JDBC_URL=jdbc:postgresql://postgresql.test.svc:5432/sprint-api", 
 								"SPRINT_API_DB_USER=postgres", 
 								"SPRINT_API_DB_PASSWORD=welcome1",
-								"SPRINT_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
-								"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"SPRINT_API_SERVICE_URI=http://sprint-api.test.svc:8080",
+								"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.test.svc:5432/project-burndown", 
 								"PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres", 
 								"PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1",
-								"PROJECT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}.svc:8080",
-								"PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"PROJECT_BURNDOWN_SERVICE_URI=http://project-burndown.test.svc:8080",
+								"PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.test.svc:5432/project-api", 
 								"PROJECT_API_DB_USER=postgres", 
 								"PROJECT_API_DB_PASSWORD=welcome1",
-								"PROJECT_API_SERVICE_URI=http://${microservice}.${project}.svc:8080" ]) {
+								"PROJECT_API_SERVICE_URI=http://project-api.test.svc:8080" ]) {
 			withMaven(mavenSettingsConfig: 'microservices-scrum') {
 		  	sh "mvn clean test"
 		  } 
