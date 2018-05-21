50897879f05f862f80f102a27da6551f777b173b
Dennis Williams <dennis.williams@estafet.com>
2018-05-21 19:26:05 +0100

    added environment variables for running tests

--- qa-pipeline.groovy
+++ qa-pipeline.groovy
@@ -7,9 +7,35 @@ node('maven') {
 	}
 
 	stage("unit tests") {
-		withMaven(mavenSettingsConfig: 'microservices-scrum') {
-	      sh "mvn clean test"
-	    } 
+		withEnv( [ 	"BASIC_UI_URI=http://${microservice}.${project}.svc:8080",
+								"TASK_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"TASK_API_DB_USER=postgres", 
+								"TASK_API_DB_PASSWORD=welcome1",
+								"TASK_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
+								"STORY_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"STORY_API_DB_USER=postgres", 
+								"STORY_API_DB_PASSWORD=welcome1",
+								"STORY_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
+								"SPRINT_BURNDOWN_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"SPRINT_BURNDOWN_DB_USER=postgres", 
+								"SPRINT_BURNDOWN_DB_PASSWORD=welcome1",
+								"SPRINT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}.svc:8080",
+								"SPRINT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"SPRINT_API_DB_USER=postgres", 
+								"SPRINT_API_DB_PASSWORD=welcome1",
+								"SPRINT_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
+								"PROJECT_BURNDOWN_REPOSITORY_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"PROJECT_BURNDOWN_REPOSITORY_DB_USER=postgres", 
+								"PROJECT_BURNDOWN_REPOSITORY_DB_PASSWORD=welcome1",
+								"PROJECT_BURNDOWN_SERVICE_URI=http://${microservice}.${project}.svc:8080",
+								"PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+								"PROJECT_API_DB_USER=postgres", 
+								"PROJECT_API_DB_PASSWORD=welcome1",
+								"PROJECT_API_SERVICE_URI=http://${microservice}.${project}.svc:8080" ]) {
+			withMaven(mavenSettingsConfig: 'microservices-scrum') {
+		  	sh "mvn clean test"
+		  } 
+		}
 	}
 
 }
