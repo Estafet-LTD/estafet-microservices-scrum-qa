0aa2be3e566ff7e40a69b45f5959bde193680a98
Steve Brown <steve.brown@estafet.com>
2019-04-10 10:16:09 +0100

    Handle failures in @Before methods.

--- src/test/java/com/estafet/microservices/scrum/cucumber/StorySteps.java
+++ src/test/java/com/estafet/microservices/scrum/cucumber/StorySteps.java
@@ -1,7 +1,10 @@
 package com.estafet.microservices.scrum.cucumber;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
 
 import com.estafet.microservices.scrum.lib.data.db.ServiceDatabases;
 import com.estafet.microservices.scrum.lib.data.project.ProjectDataSetBuilder;
@@ -32,16 +35,16 @@ public class StorySteps {
 
 	@After("@story")
 	public void after() {
-		homePage.close();
+	    Optional.ofNullable(homePage).ifPresent(pageToClose -> pageToClose.close());
 	}
-	
+
 	@Given("^the following project has already been created for add story:$")
-	public void the_following_project_has_already_been_created_for_add_story(DataTable dataTable) throws Throwable {
+	public void the_following_project_has_already_been_created_for_add_story(final DataTable dataTable) throws Throwable {
 		new ProjectDataSetBuilder().setData(dataTable.raw()).build().get(0);
 	}
 
 	@When("^I click Add Story from the project \"([^\"]*)\" page$")
-	public void i_click_Add_Story_from_the_project_page(String project) throws Throwable {
+	public void i_click_Add_Story_from_the_project_page(final String project) throws Throwable {
 		projectPage = homePage.clickHereLink().clickProjectLink(project);
 		assertTrue(projectPage.isLoaded());
 		addStoryPage = projectPage.clickAddStoryLink();
@@ -49,38 +52,38 @@ public class StorySteps {
 	}
 
 	@When("^enter a title of \"([^\"]*)\"$")
-	public void enter_a_title_of(String arg1) throws Throwable {
+	public void enter_a_title_of(final String arg1) throws Throwable {
 	   addStoryPage.setTitle(arg1);
 	}
 
 	@When("^(\\d+) story points$")
-	public void story_points(int arg1) throws Throwable {
+	public void story_points(final int arg1) throws Throwable {
 	    addStoryPage.setStoryPointsField(arg1);
 	}
-	
+
 	@When("^submit the story$")
 	public void submit_the_story() throws Throwable {
 		projectPage = addStoryPage.clickSubmitButton();
 	}
 
 	@Then("^I should be able to click on the link for story \"([^\"]*)\" navigate to the story page$")
-	public void i_should_be_able_to_click_on_the_link_for_story_navigate_to_the_story_page(String arg1) throws Throwable {
+	public void i_should_be_able_to_click_on_the_link_for_story_navigate_to_the_story_page(final String arg1) throws Throwable {
 		storyPage = projectPage.clickStoryLink(arg1);
 		assertTrue(storyPage.isLoaded());
 	}
 
 	@Then("^which should have a status of \"([^\"]*)\"$")
-	public void which_should_have_a_status_of(String arg1) throws Throwable {
+	public void which_should_have_a_status_of(final String arg1) throws Throwable {
 	    assertThat(storyPage.getStatus(), is(arg1));
 	}
 
 	@Then("^a title of \"([^\"]*)\"$")
-	public void a_title_of(String arg1) throws Throwable {
+	public void a_title_of(final String arg1) throws Throwable {
 		assertThat(storyPage.getName(), is(arg1));
 	}
 
 	@Then("^story points of (\\d+)$")
-	public void story_points_of(int arg1) throws Throwable {
+	public void story_points_of(final int arg1) throws Throwable {
 		assertThat(storyPage.getPoints(), is(arg1));
 	}
 
